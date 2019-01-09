62475ec9704321e7cbebb1a6040cf79eabf9344c
kiettisack <46478575+kiettisack@users.noreply.github.com>
2019-01-09 13:45:48 +0700

    demo
    demo
    
    Co-Authored-By: phoutthasack <phoutthasack@users.noreply.github.com>

---
+++ index.php
@@ -0,0 +1,69 @@
+<!doctype html>
+<?php include("stye.php"); ?>
+<html>
+<head>
+<meta charset="utf-8">
+
+<title>Untitled Document</title>
+
+</head>
+
+<body>
+	<?php include("menu.php");?>
+	<div class="container">
+	<?php include("slide.php")?>
+	<div class="row">
+  <div class="col-md-2">
+	<div class="list-group">
+	  <a href="#" class="list-group-item active">
+		Cras justo odio
+	  </a>
+	  <a href="#" class="list-group-item">Dapibus ac facilisis in</a>
+	  <a href="#" class="list-group-item">Morbi leo risus</a>
+	  <a href="#" class="list-group-item">Porta ac consectetur ac</a>
+	  <a href="#" class="list-group-item">Vestibulum at eros</a>
+	</div>
+  </div>
+  <div class="col-md-10">
+  	<div class="panel panel-primary">
+      <div class="panel-heading">Panel with panel-primary class</div>
+      <div class="panel-body">
+      	<div class="row">
+  <div class="col-sm-6 col-md-4">
+    <div class="thumbnail">
+      <img src="img/ดาวน์โหลด.png" alt="...">
+      <div class="caption">
+        <h3>Thumbnail label</h3>
+        <p>...</p>
+        <p><a href="#" class="btn btn-primary" role="button">Button</a> <a href="#" class="btn btn-default" role="button">Button</a></p>
+      </div>
+    </div>
+  </div>
+  <div class="col-sm-6 col-md-4">
+    <div class="thumbnail">
+      <img src="img/ดาวน์โหลด.png" alt="...">
+      <div class="caption">
+        <h3>Thumbnail label</h3>
+        <p>...</p>
+        <p><a href="#" class="btn btn-primary" role="button">Button</a> <a href="#" class="btn btn-default" role="button">Button</a></p>
+      </div>
+    </div>
+  </div>
+  <div class="col-sm-6 col-md-4">
+    <div class="thumbnail">
+      <img src="img/ดาวน์โหลด.png" alt="...">
+      <div class="caption">
+        <h3>Thumbnail label</h3>
+        <p>...</p>
+        <p><a href="#" class="btn btn-primary" role="button">Button</a> <a href="#" class="btn btn-default" role="button">Button</a></p>
+      </div>
+    </div>
+  </div>
+</div>
+      </div>
+    </div>
+  </div>
+</div>
+	</div>
+</body>
+</html>
